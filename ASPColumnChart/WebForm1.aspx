4ecf814e68a0951c2fd8a6c4cfe6cbbea4805c92
dylanfitz396 <dylanfitz396@gmail.com>
2016-11-06 22:46:53 +0000

    First Commit

---
+++ ASPColumnChart/WebForm1.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ASPColumnChart.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+<title>Column chart using Google Visualization</title>     
+     <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Literal ID="lt" runat="server"></asp:Literal>
+        <div id="chart_div"></div>    
+    </div>     
+ </form>
+</body>
+</html>
---
+++ ASPColumnChart/WebForm1.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Web.UI;
+
+namespace ASPColumnChart
+{
+    public partial class WebForm1 : Page
+    {
+        StringBuilder str = new StringBuilder();
+        //Get connection string from web.config
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings[
+            "Database1ConnectionString1"].ConnectionString);
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack == false)
+            {
+                BindChart();
+            }
+        }
+
+        private DataTable GetData()
+        {
+            var dt = new DataTable();
+            var cmd = "select Skill,ExpertiseLevel from Skills where EmpName = 'Chris'";
+            var adp = new SqlDataAdapter(cmd, conn);
+            adp.Fill(dt);
+            return dt;
+        }
+
+        private void BindChart()
+        {
+            var dt = new DataTable();
+            try
+            {
+                dt = GetData();
+
+                str.Append(@"<script type=*text/javascript*> google.load( *visualization*, *1*, {packages:[*corechart*]});
+                       google.setOnLoadCallback(drawChart);
+                       function drawChart() {
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Skill');
+        data.addColumn('number', 'ExpertiseLevel');     
+ 
+        data.addRows(" + dt.Rows.Count + ");");
+
+                for (var i = 0; i <= dt.Rows.Count - 1; i++)
+                {
+                    str.Append("data.setValue( " + i + "," + 0 + "," + "'" + dt.Rows[i]["Skill"] + "');");
+                    str.Append("data.setValue(" + i + "," + 1 + "," + dt.Rows[i]["ExpertiseLevel"] + ") ;");
+                }
+
+                //str.Append("<canvas id="+"bar"+" width="+"800"+" height="+"400"+"></canvas>");
+                str.Append(" var chart = new google.visualization.BarChart(document.getElementById('chart_div'));");
+                //str.Append(" var gradient = ctx.createLinearGradient(0, 0, 0, 400); gradient.addColorStop(0, 'rgba(151,187,205,0.7)'); gradient.addColorStop(1, 'rgba(151,187,205,0)'); ");
+                str.Append(" chart.draw(data, {width: 600, height: 500, title: 'Your Skill Chart',");
+                str.Append("hAxis: {title: 'Level of Expertise', titleTextStyle: {color: 'green'}},");
+                str.Append("colors: ['#006C01'],");
+                str.Append("animation: {duration: 1500, startup: true},");
+                str.Append("}); }");
+                str.Append("</script>");
+                lt.Text = str.ToString().Replace('*', '"');
+            }
+            catch
+            {
+            }
+        }
+    }
+}
